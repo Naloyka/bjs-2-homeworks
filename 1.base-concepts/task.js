051e19345384900fc0a6528337dd74088135fc6a
Naloyka <elena.naloeva@yandex.ru>
2022-03-16 00:30:17 +0300

    Исправлена первая задача

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,18 @@
+"use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
 
   let d = b ** 2 - 4 * a * c;
 
-  if (d < 0) {
-    return arr
+  if (d === 0) {
+    arr[0] = -b / (2 * a);
   }
-  else if (d === 0) {
-    return arr[0] = -b / (2 * a);
+  else if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
   }
-  else {
-    arr[0] = (-b + Math.sqrt(d) )/(2*a);
-    arr[1] = (-b - Math.sqrt(d) )/(2*a);
-    return arr
-  }
+  return arr
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
