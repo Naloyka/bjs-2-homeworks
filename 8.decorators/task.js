3d2dfa286817c023125d848068ec44b4f2e367c4
Naloyka <elena.naloeva@yandex.ru>
2022-04-11 19:55:07 +0300

    Решение decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,62 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+
+    result = func.call(this, ...args);
+    let hash = args.join(",");
+    let idx = cache.findIndex((item) => item.hash == hash);
+
+    if (idx !== -1) {
+
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value;
+    } else {
+
+      if (cache.length > 5) {
+        cache.shift();
+      }
+
+      cache.push({
+        'hash': hash,
+        'value': result,
+      });
+
+
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+
+
+  }
+  return wrapper
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let flag = false;
+  function wrapper(...args) {
+    if (flag) {
+      return func.call(this, ...args)
+    }
+    flag = true;
+    setTimeout(() => {
+      flag = false;
+      func.call(this, ...args);
+    }, ms)
+  }
+  return wrapper
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(debounceDecoratorNew) {
+
+  wrapper.count = 0;
+
+  function wrapper(...args) {
+    wrapper.count++;
+    return func.call(this, ...args)
+  }
+
+  return wrapper
 }
+
